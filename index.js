5de1c190f75fe4792dcdbf5944d0cef284d7f04b
MaryyamSalah <maryyamslah20@gmail.com>
2022-10-23 15:28:57 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+
+ document.addEventListener('DOMContentLoaded', function() {
+    const load = document.getElementById('text');
+   load.textContent ="This is really cool!"
+  });
+ 
